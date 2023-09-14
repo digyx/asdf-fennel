ed1566794798061e7c344bda9acc18f604c33661
Roman Godmaire <godmaire@twilit.io>
2023-09-13 21:18:53 -0400

    feat: Initial working version
    Since Fennel is distributed as a single binary, we are simply
    downloading that.  A future improvement would be to build that binary
    ourselves, but that requires a lua dependency that Fedora Silverblue
    does not have.

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for fennel.
-GH_REPO="https://github.com/digyx/fennel"
+GIT_REPO="https://git.sr.ht/~technomancy/fennel"
 TOOL_NAME="fennel"
 TOOL_TEST="fennel --version"
 
@@ -14,26 +13,15 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if fennel is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
+list_all_versions() {
+	git ls-remote --tags --refs "$GIT_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
 		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
-}
-
-list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if fennel has other means of determining installable versions.
-	list_github_tags
 }
 
 download_release() {
@@ -41,8 +29,7 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for fennel
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="https://fennel-lang.org/downloads/fennel-${version}-x86_64"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -61,7 +48,8 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert fennel executable exists.
+		mv "$install_path/$TOOL_NAME-$version" "$install_path/$TOOL_NAME"
+
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
